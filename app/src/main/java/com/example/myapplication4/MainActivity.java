e0c003cfd504682565e395f89a514cf66f4eaf96
gufooya <45197433+gufooya@users.noreply.github.com>
2018-11-25 22:00:46 +0900

    크롤 완료후 전체 체크인

--- app/src/main/java/com/example/myapplication4/MainActivity.java
+++ app/src/main/java/com/example/myapplication4/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -16,76 +17,95 @@ import org.jsoup.select.Elements;
 import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
-
-    private String htmlPageUrl = "http://www.yonhapnews.co.kr/"; //파싱할 홈페이지의 URL 주소
-    private TextView textviewHtmlDocument;
-    private String htmlContentInStringFormat = " ";
+    TextView pTextView;
+    Button   pButton;
+    String htmlPageUrl = "http://jyj.jje.hs.kr/jyj/0303/schedule?section=2"; //파싱할 홈페이지의 URL 주소
+        String contentString = " ";
+    Document doc = null;
+    Elements contents;
 
     int cnt = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textviewHtmlDocument =  (TextView) findViewById(R.id.TextView);
-        textviewHtmlDocument.setMovementMethod(new ScrollingMovementMethod()); // 스크롤 가능한 텍스트 뷰로 만들기
+        pTextView =  (TextView) findViewById(R.id.TextView);
+        pTextView.setMovementMethod(new ScrollingMovementMethod()); // 스크롤 가능한 텍스트 뷰로 만들기
 
-        Button htmlTitleButton = (Button) findViewById(R.id.Button3);
-        htmlTitleButton.setOnClickListener(new View.OnClickListener() {
+        pButton =  (Button) findViewById(R.id.Button3);
+        pButton.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
-                System.out.println(  (cnt+1)  + "번째 파싱");
-                JsoupAsyncTask jsoupAsyncTask = new JsoupAsyncTask();
-                jsoupAsyncTask.execute();
-                cnt++;
+                new DownloadFilesTask().execute();
             }
         });
     }
 
-    private class JsoupAsyncTask extends AsyncTask<Void, Void, Void> {
-
-        @Override
-        protected void onPreExecute() {
+    private class DownloadFilesTask extends AsyncTask <Void, Integer, Void> {
+        @Override protected void onPreExecute() {
             super.onPreExecute();
         }
 
         @Override
-        protected  Void doInBackground(Void... params) {
+        protected Void doInBackground(Void... params) {
             try {
-                Document doc = Jsoup.connect(htmlPageUrl).get();
 
-                //테스트1
-                Elements titles = doc.select("div.news-con h1.tit-news");
-
-                System.out.println("-------------------------------------------------");
-                for(Element e: titles) {
-                    System.out.println("title: " + e.text());
-                    htmlContentInStringFormat += e.text().trim() + "\n";
-                }
-
-                //테스트2
-                titles = doc.select("div.news-con h2.tit-news");
-
-                System.out.println("-------------------------------------------------");
-                for(Element e: titles) {
-                    System.out.println("title: " + e.text());
-                    htmlContentInStringFormat += e.text().trim() + "\n";
-                }
-
-                //테스트3
-                titles = doc.select("li.section02 div.con h2.news-t1");
-
-                System.out.println("-------------------------------------------------");
-                for(Element e: titles) {
-                    System.out.println("title: " + e.text());
-                    htmlContentInStringFormat += e.text().trim() + "\n";
-                }
-
+                doc = Jsoup.connect(htmlPageUrl).get();
+                //Log.d(">>>>>>>>>>>",doc.toString());
+                contents = doc.select("dl.event");
+                //Log.d(">>>>>>>>>>>",contents.toString());
             } catch (IOException e) {
                 e.printStackTrace();
             }
+
+            for(Element e: contents) {
+                Elements second = e.children();
+                //Log.d(">>>1111>>>>", second.html());
+
+                Elements third  = second.select("ul li");
+                //Log.d(">>>2222>>>>", third.html());
+
+                for (Element e1 : third) {
+                    Elements links = e1.getElementsByTag("a");
+
+                    Log.d(">>>>3333>>>>",  "\n");
+                    for (Element link : links) {
+                        cnt++;
+                        String linkHref = link.attr("onclick");
+                        Log.d(cnt+":" ,  " " + linkHref);
+                    }
+                }
+            //    cnt++;
+                //contentString += cnt +". "+ element.text() + "\n";
+                //if(cnt == 10)//10위까지 파싱하므로
+                //    break;
+            }
+
             return null;
+        }
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+
+        }
+
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+
+            contentString += htmlPageUrl + "\n";
+
+
+            Log.i("TAG",""+contentString);
+            pTextView.setText(contentString);
         }
     }
 }
+
+
